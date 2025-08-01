cdef6d7fe30b62c1254e581467c5894d8d8350d0
Jeevanandham R <104126169+Jeevanandham06@users.noreply.github.com>
2025-08-01 14:17:32 +0530

    Initial commit

---
+++ src/com/hashing/Hashing.java
@@ -0,0 +1,24 @@
+package com.hashing;
+
+import java.util.HashMap;
+
+public class Hashing {
+    public static  void main(String[]args){
+        int [] nums ={4,1,2,1,2};
+        int result=finduniqueElement (nums);
+         System.out.println("Unique Element: "+result);
+    }
+   public  static int finduniqueElement(int[]nums){
+        HashMap<Integer ,Integer>map=new HashMap<>();
+        for(int num :nums){
+            map.put(num, map.getOrDefault(num,0)+1);
+
+        }
+        for(int num:nums){
+            if(map.get(num)==1){
+                return  num;
+            }
+        }
+        return -1;
+   }
+}
---
+++ src/com/hashing/Sumoffreq.java
@@ -0,0 +1,36 @@
+package com.hashing;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Sumoffreq {
+    static class Solution {
+        public int sumHighestAndLowestFrequency(int[] nums) {
+            HashMap<Integer, Integer> map = new HashMap<>();
+
+            for (int num : nums) {
+                map.put(num, map.getOrDefault(num, 0) + 1);
+            }
+
+            int max = Integer.MIN_VALUE;
+            int min = Integer.MAX_VALUE;
+
+            for (int freq : map.values()) {
+                max = Math.max(max, freq);
+                min = Math.min(min, freq);
+            }
+
+            return max + min;
+        }
+    }
+    public static  void main(String[]args){
+        Solution solution = new Solution();
+        int[] nums1 = {1, 2, 2, 3, 3, 3};
+        int[] nums2 = {4, 4, 5, 5, 6};
+
+        System.out.println(solution.sumHighestAndLowestFrequency(nums1));
+        System.out.println(solution.sumHighestAndLowestFrequency(nums2));
+    }
+}
+
+
