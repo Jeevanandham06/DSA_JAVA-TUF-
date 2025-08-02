758f3a559d8b274d3d96d7a069d6f5e4adfc8315
Jeevanandham R <104126169+Jeevanandham06@users.noreply.github.com>
2025-08-02 11:45:12 +0530

    Initial commit

---
+++ src/com/hashing/Highestoccuring.java
@@ -0,0 +1,38 @@
+package com.hashing;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Highestoccuring {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 2, 3, 3, 3,};
+        int result = Solution.highestOccuring(nums);
+        System.out.print("Highest Occuring element of the array:" + result);
+    }
+
+    class Solution {
+        public static int highestOccuring(int[] nums) {
+            HashMap<Integer, Integer> map = new HashMap<>();
+            for (int num : nums) {
+                map.put(num, map.getOrDefault(num, 0) + 1);
+            }
+            int maxFreq = Integer.MIN_VALUE;
+            int maxele = Integer.MAX_VALUE;
+
+            for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+
+
+                if (entry.getValue() > maxFreq) {
+                    maxFreq = entry.getValue();
+                    maxele = entry.getKey();
+                } else if (entry.getValue() == maxFreq) {
+                    maxele = Math.min (maxele, entry.getKey());
+                }
+
+
+            }
+            return maxele;
+        }
+    }
+}
+
